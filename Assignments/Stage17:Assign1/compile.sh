1604ce8e05deede4eda8abbca4b29b86c78feed6
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2019-09-15 20:58:19 +0530

    Partial Stage17

---
+++ Assignments/Stage17:Assign1/compile.sh
@@ -0,0 +1,14 @@
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/sched_module.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/timer.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/boot_module.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/haltprog.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/sched_module.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/os_startup.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/MOD_0.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/MOD_4.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/sample_int7.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/console_interr.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/int_6.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/MOD_1.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/MOD_2.spl
+./spl /root/Desktop/myexpos/Assignments/Stage17:Assign1/int_9.spl
