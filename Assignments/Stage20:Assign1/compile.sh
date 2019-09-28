eb0134bdd3f3b4adf1f5802b82642d8896221707
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2019-09-28 22:39:44 +0530

    Stage20:Assign1

---
+++ Assignments/Stage20:Assign1/compile.sh
@@ -0,0 +1,16 @@
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/sched_module.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/timer.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/boot_module.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/haltprog.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/sched_module.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/os_startup.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/MOD_0.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/MOD_4.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/sample_int7.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/console_interr.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/disk_int.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/int_6.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/MOD_1.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/MOD_2.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/int_9.spl
+./spl /root/Desktop/myexpos/Assignments/Stage19:Assign1/exception_handler.spl
